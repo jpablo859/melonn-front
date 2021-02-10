f6345a91e5562986143981b29066a17ecba04378
Juan Pablo Saldarriaga <cic04@citropical.com.co>
2021-02-10 17:28:35 -0500

    api ok -- cors

---
+++ src/components/newOrder/InfoOrder.js
@@ -0,0 +1,111 @@
+import  React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export const InfoOrder = () => {
+
+    const API_KEY = '8hu71URNzm7FCLV9LfDPd9Gz61zN2diV6kG2hDEw';
+
+    const [shpMethod, setShpMethod] = useState();
+
+    const [state, setState] = useState({
+        seller: '',
+        shippingMethod: '',
+        orderNum: '',
+        buyerName: '',
+        buyerPhone: '',
+        buyerEmail: ''
+    });
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(`https://yhua9e1l30.execute-api.us-east-1.amazonaws.com/sandbox/shipping-methods`, {
+                    headers: {
+                        'x-api-key': API_KEY
+                    }
+                });
+                setShpMethod(response.data);
+            } catch (err) {
+
+            }
+        })()
+    }, [])
+    
+    const {orderNum, buyerName, buyerPhone, buyerEmail, seller, shippingMethod} = state;
+
+    const fnChange = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <table className="table">
+                <thead>
+                    <tr>
+                        <th>Seller</th>
+                        <th>Shipping Method</th>
+                        <th>Order Number</th>
+                        <th>Buyer Full Name</th>
+                        <th>Buyer Phone Number</th>
+                        <th>Buyer Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="seller"
+                                value={seller}
+                                onChange={fnChange}
+                            />
+                        </td>
+                        <td>
+                            <select 
+                                className="form-control"
+                                onChange={fnChange}
+                                name="shippingMethod"
+                            >
+                                <option key={0}></option>
+                                {shpMethod && shpMethod.map(({id, name}) => <option key={id}>{name}</option>)}
+                            </select>
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="orderNum"
+                                value={orderNum}
+                                onChange={fnChange}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerName"
+                                value={buyerName}
+                                onChange={fnChange}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerPhone"
+                                value={buyerPhone}
+                                onChange={fnChange}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerEmail"
+                                value={buyerEmail}
+                                onChange={fnChange}
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+    )
+}
--- src/components/newOrder/NewOrder.js
+++ src/components/newOrder/NewOrder.js
@@ -1,10 +1,94 @@
-import React from 'react'
+import { InfoOrder } from './InfoOrder';
 
 export const NewOrder = () => {
+
     
+
     return (
-        <div>
-            <h1>New Order</h1>
+        <div className="container-fluid">
+            <h1 className="text-center">New Order</h1>
+            <InfoOrder/>
+                
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Shipping Adress</th>
+                        <th>Shipping City</th>
+                        <th>Shipping Region</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="orderNum"
+                                
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerName"
+                                
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerPhone"
+                                
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            {/* <table className="table">
+                <thead>
+                    <tr>Items</tr>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Quantity</th>
+                        <th>Weright</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="orderNum"
+                                value={orderNum}
+                                
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerName"
+                                value={buyerName}
+                                
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerPhone"
+                                value={buyerPhone}
+                                
+                            />
+                        </td>
+                        <td>
+                            <input
+                                className="form-control"
+                                name="buyerEmail"
+                                value={buyerEmail}
+                                
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table> */}
         </div>
     )
 }
